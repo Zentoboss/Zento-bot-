47e06acc19e4f84a5286f425545f5fc7b3fabb3c
Zentoboss <sultanzaffiro@gmail.com>
2025-04-20 17:22:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import telebot
 
-bot = telebot.TeleBot("ВАШ_7651020708:AAE55lbMYpgBvNe82XLUnheBgWf1wleqchIТОКЕН")
+bot = telebot.TeleBot("ВАШ_Т7651020708:AAE55lbMYpgBvNe82XLUnheBgWf1wleqchIОКЕН")
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет, я ZentoBot!")
+    bot.reply_to(message, "Привет! Я бот.")
 
 bot.polling()
